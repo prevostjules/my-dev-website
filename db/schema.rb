201eb35db7b3c03cd992035ef6d60341ffd3f383
Jules Prevost <jprevostdev@gmail.com>
2020-07-22 18:32:31 +0200

    projects back end is working. Multiple pictures can be load for banner and in the text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_152356) do
+ActiveRecord::Schema.define(version: 2020_07_22_160739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2020_07_22_152356) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -72,4 +74,5 @@ ActiveRecord::Schema.define(version: 2020_07_22_152356) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "projects", "users"
 end

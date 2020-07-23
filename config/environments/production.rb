bcc86613ddf3628b2925dd908bb3637737feceb5
Jules Prevost <jprevostdev@gmail.com>
2020-07-23 19:24:57 +0200

    d

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.active_storage.service = :cloudinary
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -92,6 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.active_storage.service = :cloudinary
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
